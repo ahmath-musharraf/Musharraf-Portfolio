fa4bb613ec5fcc5c2d00142e6ec0fd4af8102544
Ahmath Musharraf <97532212+ahmathmusharraf@users.noreply.github.com>
2026-04-28 16:13:49 +0400

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -251,7 +251,7 @@ export const VISUAL_WORKS: VisualWork[] = [
     title: "Jetour Upgrades New Style", 
     category: "Videography", 
     imageUrl: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=800&auto=format&fit=crop",
-    videoUrl: "https://www.youtube.com/ldzhMWha3bk?si=bJMrmXBTXo46I20A", 
+    videoUrl: "https://youtu.be/ldzhMWha3bk?si=3L6CzMCPDAE4hlOD", 
     aspectRatio: '16:9'
   },
   { 
